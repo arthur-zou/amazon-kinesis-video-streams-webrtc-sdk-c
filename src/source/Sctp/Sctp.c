e2b026f812bf8be222b29211a86fc2e51eb52990
disa6302 <disa6302@colorado.edu>
2020-06-01 10:09:53 -0700

    Correcting partial reliability

--- src/source/Sctp/Sctp.c
+++ src/source/Sctp/Sctp.c
@@ -175,6 +175,21 @@ STATUS sctpSessionWriteMessage(PSctpSession pSctpSession, UINT32 streamId, BOOL 
     spa.sendv_flags |= SCTP_SEND_SNDINFO_VALID;
     spa.sendv_sndinfo.snd_sid = streamId;
 
+    if(pSctpSession->pPacket[1] & DCEP_DATA_CHANNEL_RELIABLE_UNORDERED) {
+        spa.sendv_sndinfo.snd_flags |= SCTP_UNORDERED;
+    }
+    if(pSctpSession->pPacket[1] & DCEP_DATA_CHANNEL_REXMIT) {
+        spa.sendv_prinfo.pr_policy = SCTP_PR_SCTP_RTX;
+        MEMCPY(&spa.sendv_prinfo.pr_value, pSctpSession->pPacket+4, SIZEOF(UINT32));
+        putUnalignedInt32BigEndian(&spa.sendv_prinfo.pr_value, spa.sendv_prinfo.pr_value);
+    }
+    if(pSctpSession->pPacket[1] & DCEP_DATA_CHANNEL_TIMED) {
+        spa.sendv_prinfo.pr_policy = SCTP_PR_SCTP_TTL;
+        spa.sendv_prinfo.pr_value = *((PUINT32)(pSctpSession->pPacket+4));
+        MEMCPY(&spa.sendv_prinfo.pr_value, pSctpSession->pPacket+4, SIZEOF(UINT32));
+        putUnalignedInt32BigEndian(&spa.sendv_prinfo.pr_value, spa.sendv_prinfo.pr_value);
+    }
+
     putInt32((PINT32) &spa.sendv_sndinfo.snd_ppid, isBinary ? SCTP_PPID_BINARY : SCTP_PPID_STRING);
     CHK(usrsctp_sendv(pSctpSession->socket, pMessage, pMessageLen, NULL, 0, &spa, SIZEOF(spa), SCTP_SENDV_SPA, 0) > 0, STATUS_INTERNAL_ERROR);
 
@@ -212,57 +227,48 @@ STATUS sctpSessionWriteDcep(PSctpSession pSctpSession, UINT32 streamId, PCHAR pC
      CHK(pSctpSession != NULL && pChannelName != NULL, STATUS_NULL_ARG);
      CHK((pPacket = (PBYTE) MEMCALLOC(1, pPacketSize)) != NULL, STATUS_NOT_ENOUGH_MEMORY);
 
+     MEMSET(&spa, 0x00, SIZEOF(struct sctp_sendv_spa));
+     MEMSET(pSctpSession->pPacket, 0x00, SIZEOF(pSctpSession->pPacket));
+     pSctpSession->pPacketSize = SCTP_DCEP_HEADER_LENGTH + pChannelNameLen;
      /* Setting the fields of DATA_CHANNEL_OPEN message */
 
      pPacket[0] = DCEP_DATA_CHANNEL_OPEN; // message type
 
      // Set Channel type based on supplied parameters
-     pPacket[1] = DCEP_DATA_CHANNEL_RELIABLE;
+     pPacket[1] = DCEP_DATA_CHANNEL_RELIABLE_ORDERED;
 
-     // Set channel type and reliability parameters based on input
-     // SCTP allows fine tuning the channel robustness:
-     // 1. Ordering: The data packets can be sent out in an ordered/unordered fashion
-     // 2. Reliability: This determines how the retransmission of packets is handled.
-     // There are 2 parameters that can be fine tuned to achieve this:
-     //     a. Number of retransmits
-     //     b. Packet lifetime
-     // Default values for the parameters is 0. This falls back to reliable channel
+     //   Set channel type and reliability parameters based on input
+     //   SCTP allows fine tuning the channel robustness:
+     //      1. Ordering: The data packets can be sent out in an ordered/unordered fashion
+     //      2. Reliability: This determines how the retransmission of packets is handled.
+     //   There are 2 parameters that can be fine tuned to achieve this:
+     //      a. Number of retransmits
+     //      b. Packet lifetime
+     //   Default values for the parameters is 0. This falls back to reliable channel
+          spa.sendv_flags |= SCTP_SEND_PRINFO_VALID;
 
-     if(pRtcDataChannelInit->ordered) {
-        if(pRtcDataChannelInit->maxRetransmits > 0) {
-            pPacket[1] = DCEP_DATA_CHANNEL_PARTIAL_RELIABLE_REXMIT;
-            putUnalignedInt16BigEndian(pPacket + 6, pRtcDataChannelInit->maxRetransmits);
-        }
-        else if(pRtcDataChannelInit->maxPacketLifeTime > 0) {
-            pPacket[1] = DCEP_DATA_CHANNEL_PARTIAL_RELIABLE_TIMED;
-            putUnalignedInt16BigEndian(pPacket + 6, pRtcDataChannelInit->maxPacketLifeTime);
-         }
-     }
-     else {
-         spa.sendv_sndinfo.snd_flags = SCTP_UNORDERED;
-         if(pRtcDataChannelInit->maxRetransmits > 0) {
-             pPacket[1] = DCEP_DATA_CHANNEL_PARTIAL_RELIABLE_REXMIT_UNORDERED;
-             putUnalignedInt16BigEndian(pPacket + 6, pRtcDataChannelInit->maxRetransmits);
-         }
-         else if(pRtcDataChannelInit->maxPacketLifeTime > 0) {
-             pPacket[1] = DATA_CHANNEL_PARTIAL_RELIABLE_TIMED_UNORDERED;
-             putUnalignedInt16BigEndian(pPacket + 6, pRtcDataChannelInit->maxPacketLifeTime);
-         }
-         else {
-             pPacket[1] = DCEP_DATA_CHANNEL_RELIABLE_UNORDERED;
-         }
-     }
+          if(!pRtcDataChannelInit->ordered) {
+              pSctpSession->pPacket[1] |= DCEP_DATA_CHANNEL_RELIABLE_UNORDERED;
+          }
+          // TODO: Set the other to NULL when both are specified
+          if(pRtcDataChannelInit->maxRetransmits.value >= 0 && pRtcDataChannelInit->maxRetransmits.isNull == FALSE) {
+              pSctpSession->pPacket[1] |= DCEP_DATA_CHANNEL_REXMIT;
+              putUnalignedInt32BigEndian(pSctpSession->pPacket + 4, pRtcDataChannelInit->maxRetransmits.value);
+          }
+          else if(pRtcDataChannelInit->maxPacketLifeTime.value >= 0 && pRtcDataChannelInit->maxPacketLifeTime.isNull == FALSE) {
+              pSctpSession->pPacket[1] |= DCEP_DATA_CHANNEL_TIMED;
+              putUnalignedInt32BigEndian(pSctpSession->pPacket + 4, pRtcDataChannelInit->maxPacketLifeTime.value);
+          }
 
-     DLOGD("PARTIAL RELIABILITY PARAMS: 0x%02x [CHANNEL_TYPE] 0x%02x%02x%02x%02x [PARAMETER]", pPacket[1], pPacket[4], pPacket[5], pPacket[6], pPacket[7]);
-     putUnalignedInt16BigEndian(pPacket + SCTP_DCEP_LABEL_LEN_OFFSET, pChannelNameLen);
-     MEMCPY(pPacket + SCTP_DCEP_LABEL_OFFSET, pChannelName, pChannelNameLen);
-     MEMSET(&spa, 0x00, SIZEOF(struct sctp_sendv_spa));
-     spa.sendv_flags |= SCTP_SEND_SNDINFO_VALID;
-     spa.sendv_sndinfo.snd_sid = streamId;
+          DLOGD("IN DCEP PARTIAL RELIABILITY PARAMS: 0x%02x [CHANNEL_TYPE] 0x%02x%02x%02x%02x [PARAMETER]", pSctpSession->pPacket[1], pSctpSession->pPacket[4], pSctpSession->pPacket[5], pSctpSession->pPacket[6], pSctpSession->pPacket[7]);;
+          putUnalignedInt16BigEndian(pSctpSession->pPacket + SCTP_DCEP_LABEL_LEN_OFFSET, pChannelNameLen);
+          MEMCPY(pSctpSession->pPacket + SCTP_DCEP_LABEL_OFFSET, pChannelName, pChannelNameLen);
+          spa.sendv_flags |= SCTP_SEND_SNDINFO_VALID;
+          spa.sendv_sndinfo.snd_sid = streamId;
 
-     putInt32((PINT32) &spa.sendv_sndinfo.snd_ppid, SCTP_PPID_DCEP);
-     CHK(usrsctp_sendv(pSctpSession->socket, pPacket, pPacketSize, NULL, 0, &spa, SIZEOF(spa), SCTP_SENDV_SPA, 0) > 0, STATUS_INTERNAL_ERROR);
+          putInt32((PINT32) &spa.sendv_sndinfo.snd_ppid, SCTP_PPID_DCEP);
 
+          CHK(usrsctp_sendv(pSctpSession->socket, pSctpSession->pPacket, pSctpSession->pPacketSize, NULL, 0, &spa, SIZEOF(spa), SCTP_SENDV_SPA, 0) > 0, STATUS_INTERNAL_ERROR);
 CleanUp:
     SAFE_MEMFREE(pPacket);
 
